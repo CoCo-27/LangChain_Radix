964e9fc6ceffe2d7fbf4ade3516dd54115cd2e9b
CoCo <122005116+CoCo-27@users.noreply.github.com>
2023-06-08 10:22:09 -0400

    Initialize feature/animate

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // experimental: {
+  //   fontLoaders: [
+  //     {
+  //       loader: "@next/font/google",
+  //     },
+  //   ],
+  // },
+}
+
+export default nextConfig
